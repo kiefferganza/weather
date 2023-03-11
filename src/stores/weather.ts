9f49ea757390227bcb8c63449bbfe60f54b9f126
Kieffer <kganza@traxiontech.net>
2023-03-11 22:11:54 +0800

    removed hardcoded param

--- src/stores/weather.ts
+++ src/stores/weather.ts
@@ -7,7 +7,7 @@ export const useWeatherStore = defineStore('weather', () => {
     const forecast = ref([])
 
     async function searchForecast(search: String) {
-        await axios.get(`https://api.weatherapi.com/v1/forecast.json?key=${import.meta.env.VITE_API_KEY}&q=davao&days=1&aqi=no&alerts=no`, {
+        await axios.get(`https://api.weatherapi.com/v1/forecast.json?key=${import.meta.env.VITE_API_KEY}`, {
             params: {
                 q: search,
                 days: 1,
